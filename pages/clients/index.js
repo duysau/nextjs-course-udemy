81d946c080c608f718b68cd71fca8f8041cf6b98
hoan nguyen <hoannv.dev@gmail.com>
2022-01-05 01:05:04 +0700

    done router

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,11 +1,10 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const ClientsPage = () => {
-
   const clients = [
-    {id: 'max', name: 'Maximilian'},
-    {id: 'manu', name: 'Manuel'}
-  ]
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
 
   console.log();
 
@@ -13,9 +12,18 @@ const ClientsPage = () => {
     <div>
       <h1>The Client Page</h1>
       <ul>
-        {clients.map(client => <li key={client.id}>
-          <Link href={`/clients/${client.id}`}>{client.name}</Link>
-        </li>)}
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
